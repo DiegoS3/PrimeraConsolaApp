eb3c7ff2af5e9017fe2388ce072101703a4460f1
DiegoS3 <diegozj92@gmail.com>
2020-09-23 10:52:26 +0200

    Mensaje en castellano

---
+++ PrimeraConsolaApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrimeraConsolaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Esto es un comentario
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
